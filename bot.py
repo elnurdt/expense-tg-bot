3615f365f77ee422cfa8f7c2a1ddfd10d51dd06c
Elnur <elnurab068@gmail.com>
2026-07-18 19:36:12 +0500

    Add interactive expense deletion via inline keyboard

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from dotenv import load_dotenv
 from database import get_expenses, save_expenses
+import logging
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardButton
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
@@ -30,6 +35,44 @@ async def show_total(message: types.Message):
         await message.answer(text)
     else:
         await message.answer('Список пустой (')        
+
+
+@dp.message(Command('delete'))
+async def process_delete(message: types.Message):
+    user_id = str(message.from_user.id)
+    db_data = get_expenses()
+    user_expenses = db_data.get(user_id, [])
+
+    if user_expenses:
+        builder = InlineKeyboardBuilder()
+
+        for index, expense in enumerate(user_expenses):
+            builder.button(
+                text=f"{expense['name']} ({expense['amount']}тг)",
+                callback_data=f'delete_{index}'
+            )
+        builder.adjust(1)
+
+        await message.answer("Выбери трату для удаления:", reply_markup=builder.as_markup())
+    else:
+        await message.answer("Нечего удалять")    
+
+
+@dp.callback_query(F.data.startswith('delete_'))
+async def process_delete_callback(callback: types.CallbackQuery):
+    user_id = str(callback.from_user.id)
+    delete_index = int(callback.data.split('_')[1])
+    db_data = get_expenses()
+    user_expenses = db_data[user_id]
+
+    #Удаление выбранной записи
+    user_expenses.pop(delete_index)
+
+    #Сохранение изменения
+    save_expenses(db_data)
+
+    await callback.answer("Удалено!")
+    await callback.message.edit_text("Трата успешно удалена!")
 
 
 @dp.message()
