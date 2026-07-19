5b8660c9e22766b7bd05908473c48c578bf95673
Elnur <elnurab068@gmail.com>
2026-07-19 22:51:11 +0500

    Use with for SQLite connections

--- database.py
+++ database.py
@@ -3,44 +3,30 @@ import sqlite3
 DB_PATH = 'expenses.db'
 
 def init_db():
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    #Create table
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS expenses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id TEXT,
-            name TEXT,
-            amount INTEGER,
-            created_at TEXT DEFAULT CURRENT_TIMESTAMP
-                   )
-''')
-    
-    #Save and close conection
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id TEXT,
+                name TEXT,
+                amount INTEGER,
+                created_at TEXT DEFAULT CURRENT_TIMESTAMP
+                    )
+        ''')
 
 
 def add_expense(user_id: str, name: str, amount: int):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute('INSERT INTO expenses (user_id, name, amount) VALUES (?, ?, ?)', (user_id, name, amount))
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO expenses (user_id, name, amount) VALUES (?, ?, ?)', (user_id, name, amount))
 
 
 def get_user_expenses(user_id: str) -> list[dict]:
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT id, name, amount FROM expenses WHERE user_id = ?', (user_id,))
-    
-    rows = cursor.fetchall()
-    
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, name, amount FROM expenses WHERE user_id = ?', (user_id,))
+        rows = cursor.fetchall()
 
     expenses = []
     for row in rows:
@@ -54,20 +40,12 @@ def get_user_expenses(user_id: str) -> list[dict]:
 
 
 def delete_expense(expense_id: int):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute('DELETE FROM expenses WHERE id = ?', (expense_id,))
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM expenses WHERE id = ?', (expense_id,))
 
 
 def clear_expenses(user_id: str):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-
-    cursor.execute('DELETE FROM expenses WHERE user_id = ?', (user_id,))
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM expenses WHERE user_id = ?', (user_id,))
